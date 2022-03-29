e14533b51c79906843a42e8d88e2c971c1adb4c5
Vct0r <pxdisorder@gmail.com>
2022-03-29 17:14:52 -0300

    miercoles
    Servicios, horario, oferta 2 y testimonios

--- php/sendmail.php
+++ php/sendmail.php
@@ -1,7 +1,7 @@
 <?php
 if($_POST)
 {
-	$to_email   	= "youremail@email.com"; //Recipient email, Replace with own email here
+	$to_email   	= "contacto@canclan.cl"; //Recipient email, Replace with own email here
 	
 	//check if its an ajax request, exit if not
     if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
